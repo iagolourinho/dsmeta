932a1e56a1fcee3128043027a4af22faffebf170
Iago Lourinho <iagfeli@hotmail.com>
2022-07-13 00:39:33 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import NotificationButton from "./components/notificationButton"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
-return(
-  <>
-  <h1>Olá Mundo</h1>
-  <NotificationButton />
-  </>
-)
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import icon from '../../assets/img/notification-icon.svg'
 
 import './styles.css'
